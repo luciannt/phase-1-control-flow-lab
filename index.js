36fab3667b3a648d0ebff2e5384ebdab75cf26be
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-08 16:28:51 -0500

    commit tree 53325d6f491349b12b2eb18a57823ec91cb0f837

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sample) {
+  if (sample <= 400) {
+    return "This one is on me!";
+  } else if (sample >= 400 && sample < 2000) {
+    return "That will be twenty bucks.";
+  } else if (sample > 2000 && sample < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (sample > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  if (destination === "NYC") {
+    return "Ok, sounds good.";
+  } else if (destination !== "NYC") {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "not as generous":
+      return "Thank you.";
+      break;
+    case "generous":
+      return "Thank you so much.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
